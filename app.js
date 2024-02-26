5556945e358efc945bc6d0c1d69c4637e77448d8
AlexisGuerraA <alexis@excite.com.mx>
2024-02-26 02:41:50 -0600

    feat: cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {connectToDB} = require('./src/config/mongo.db.js')
 const Routes = require('./src/routes/index.js')
+const cors = require('cors');
 
 var app = express();
 connectToDB
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 
